0559c45b2fe42f334b75a3a8bc422b5c0e77cc51
dhruvmoradiya108 <dhruv.moradiya108@gmail.com>
2024-08-24 13:07:29 +0530

    Save changes

--- src/app/components/us/us.component.ts
+++ src/app/components/us/us.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-us',
@@ -7,6 +7,27 @@ import { Component } from '@angular/core';
   templateUrl: './us.component.html',
   styleUrl: './us.component.css'
 })
-export class UsComponent {
+export class UsComponent implements OnInit {
 
+  ngOnInit(): void {
+    this.checkScroll();
+  }
+
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    this.checkScroll();
+  }
+
+
+  checkScroll() {
+    const teamInfo = document.querySelector('.team-info') as HTMLElement;
+    const teamInfoPosition = teamInfo.getBoundingClientRect().top;
+    const screenPosition = window.innerHeight / 1.3;
+
+    if (teamInfoPosition < screenPosition) {
+      teamInfo.classList.add('active');
+    } else {
+      teamInfo.classList.remove('active');
+    }
+  } 
 }
