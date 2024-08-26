9bf8ea07263d5f98e9ef8b1993ebcbbdeb214530
dhruvmoradiya108 <dhruv.moradiya108@gmail.com>
2024-08-26 05:36:37 +0530

    Save changes

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
@@ -9,35 +9,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  words: string[] = ['quality', 'speed', 'innovation', 'excellence'];
-  currentWordIndex: number = 0;
-  charIndex: number = 0;
-  isDeleting: boolean = false;
-  displayedText: string = '';
+  // words: string[] = ['quality', 'speed', 'innovation', 'excellence'];
+  // currentWordIndex: number = 0;
+  // charIndex: number = 0;
+  // isDeleting: boolean = false;
+  // displayedText: string = '';
 
   constructor() { }
 
+  // ngOnInit(): void {
+  //   this.type();
+  // }
+
+  // type(): void {
+  //   const currentWord = this.words[this.currentWordIndex];
+  //   this.displayedText = currentWord.slice(0, this.charIndex);
+
+  //   if (!this.isDeleting && this.charIndex < currentWord.length) {
+  //     this.charIndex++;
+  //     setTimeout(() => this.type(), 150);
+  //   } else if (this.isDeleting && this.charIndex > 0) {
+  //     this.charIndex--;
+  //     setTimeout(() => this.type(), 100);
+  //   } else {
+  //     this.isDeleting = !this.isDeleting;
+  //     if (!this.isDeleting) {
+  //       this.currentWordIndex = (this.currentWordIndex + 1) % this.words.length;
+  //     }
+  //     setTimeout(() => this.type(), 1500);
+  //   }
+  // }
+
   ngOnInit(): void {
-    this.type();
-  }
-
-  type(): void {
-    const currentWord = this.words[this.currentWordIndex];
-    this.displayedText = currentWord.slice(0, this.charIndex);
-
-    if (!this.isDeleting && this.charIndex < currentWord.length) {
-      this.charIndex++;
-      setTimeout(() => this.type(), 150);
-    } else if (this.isDeleting && this.charIndex > 0) {
-      this.charIndex--;
-      setTimeout(() => this.type(), 100);
-    } else {
-      this.isDeleting = !this.isDeleting;
-      if (!this.isDeleting) {
-        this.currentWordIndex = (this.currentWordIndex + 1) % this.words.length;
-      }
-      setTimeout(() => this.type(), 1500);
-    }
   }
 
 }
